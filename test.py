f3f450f97188dd17c7f38da6574dc8168361752c
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-02-02 23:43:14 +0100

    getting links for all text files

--- test.py
+++ test.py
@@ -93,3 +93,40 @@ for process in as_completed(processes):
 unpacked_sub_dir_list = [item for each_dir in sub_dir_list for item in each_dir]
 
 sub_dir_df = pd.DataFrame(unpacked_sub_dir_list).to_csv('total_sub_directories.txt',header=False,index=False)
+
+#get links for all text files
+
+start = time()
+
+all_dir_link = pd.read_csv(r'total_sub_directories.txt',header=None)[0].tolist()
+
+text_links = []
+
+def get_text_links(links):
+    url = 'http://ohhla.com/' + links
+    soup = BeautifulSoup(requests.get(url).text,'html.parser')
+    gross_links = [links + link['href'] for link in soup.find_all('a',href=True) if '.txt' in link['href']]
+    return gross_links
+
+processes = []
+
+with ThreadPoolExecutor(max_workers=10) as executor:
+    for link in all_dir_link:
+        processes.append(executor.submit(get_links,link))
+
+for process in as_completed(processes):
+    links_per_total_links.append(process.result())
+
+unpacked_text_links = [item for each_dir in text_links for item in each_dir]
+
+end = time()
+
+print(end-start)
+
+pd.DataFrame(unpacked_text_links).to_csv('total_text_links.txt',header=False,index=False)
+
+#scraping lyrics
+
+start = time()
+
+all_text_links = pd.read_csv(r'total_text_links.txt',header=None)[0].tolist()
